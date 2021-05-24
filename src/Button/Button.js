68405e30741741392ae057bedf2759e6c5345aec
Zhenia <Zhenia>
2021-05-24 10:36:18 +0200

    button prevent default

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -3,7 +3,9 @@ import classes from "./Button.module.css";
 const Button = (props) => {
   return (
     <div className={classes.Button}>
-      <a href={props.to}>{props.children}</a>
+      <a href={props.to} onclick="event.preventDefault()">
+        {props.children}
+      </a>
     </div>
   );
 };
