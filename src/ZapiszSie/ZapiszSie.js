ac0f3bf2f94886ee181a66a0f5bf31ad676e4230
Zhenia <Zhenia>
2021-06-01 16:54:03 +0200

    add kommentarze FAQ GIFT

--- src/ZapiszSie/ZapiszSie.js
+++ src/ZapiszSie/ZapiszSie.js
@@ -5,14 +5,12 @@ import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
 
 const ZapiszSie = () => {
-  console.log(value);
-
   return (
     <div>
       <div className={classes.Photo}></div>
       <div className={homeClasses.Home_content}>
         <h1>Wybierz Dowolny Termin</h1>
-        <Calendar locale="PL-pl" calendarType={"ISO 8601"} value={value} />
+        <Calendar locale="PL-pl" calendarType={"ISO 8601"} />
       </div>
     </div>
   );
