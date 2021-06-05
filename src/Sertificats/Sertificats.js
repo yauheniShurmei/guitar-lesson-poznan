c8ff555554560afe25174800277467a6ee7236e6
Zhenia <Zhenia>
2021-06-05 21:22:21 +0200

    certyfikaty dodalem

--- src/Sertificats/Sertificats.js
+++ src/Sertificats/Sertificats.js
@@ -10,25 +10,25 @@ const Sertificats = () => {
       <h1>Certyfikaty</h1>
       <div className={classes.Certyfikaty}>
         <div>
-          <img src={certyfikat_01_640x425} />
+          <img src={certyfikat_01_640x425} alt="guitar lesson" />
           <p>50zł</p>
           <h3>Jedna Lekcja Gitary</h3>
           <span>Jedna godzina (60 minut) zajęć</span>
         </div>
         <div>
-          <img src={certyfikat_02_640x425} />
+          <img src={certyfikat_02_640x425} alt="guitar lesson" />
           <p>100zł</p>
           <h3>Dwie Lekcja Gitary</h3>
           <span>Dwie godzina (60 minut) zajęć</span>
         </div>
         <div>
-          <img src={certyfikat_03_640x425} />
+          <img src={certyfikat_03_640x425} alt="guitar lesson" />
           <p>150zł</p>
           <h3>Trzy Lekcja Gitary</h3>
           <span>Trzy godzina (60 minut) zajęć</span>
         </div>
         <div>
-          <img src={certyfikat_04_640x425} />
+          <img src={certyfikat_04_640x425} alt="guitar lesson" />
           <p>200zł</p>
           <h3>Cztery Lekcja Gitary</h3>
           <span>Cztery godzina (60 minut) zajęć</span>
