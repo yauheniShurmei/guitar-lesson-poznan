d03332cc384efe89de17a3c86548c691b1fc05d4
Zhenia <Zhenia>
2021-06-07 14:17:48 +0200

    dodalem listanie stron w materialach

--- src/Materialy/Materialy.js
+++ src/Materialy/Materialy.js
@@ -2,21 +2,63 @@ import classes from "../Styles/MainStyle.module.css";
 import one_book from "../assets/img/books/one_book.jpg";
 import one_book_02 from "../assets/img/books/one_book_02.jpg";
 import one_book_03 from "../assets/img/books/one_book_03.jpg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Backdrop from "../Backdrop/Backdrop";
+import firsPage from "../assets/img/some_pages_from_book/03.jpg";
+import secPage from "../assets/img/some_pages_from_book/04.jpg";
+import thirdPage from "../assets/img/some_pages_from_book/05.jpg";
+import foursPage from "../assets/img/some_pages_from_book/06.jpg";
+import fifthPage from "../assets/img/some_pages_from_book/07.jpg";
+import sixthPage from "../assets/img/some_pages_from_book/08.jpg";
 
 const Materialy = () => {
+  const [pages, setPages] = useState([
+    one_book,
+    firsPage,
+    secPage,
+    thirdPage,
+    foursPage,
+    fifthPage,
+    sixthPage,
+  ]);
   const [isOpen, setIsOpen] = useState(false);
   const [bookIndex, setBookIndex] = useState();
+  const [index, setIndex] = useState(0);
 
   const clickHandler = (bookIndex) => {
-    setIsOpen((prevState) => !prevState);
-    setBookIndex(bookIndex);
+    if (bookIndex) {
+      setIsOpen((prevState) => !prevState);
+      setBookIndex(bookIndex);
+      const newPages = [...pages];
+      newPages.splice(0, 1, bookIndex);
+      setPages(newPages);
+      console.log(pages);
+    } else {
+      setIsOpen((prevState) => !prevState);
+      setIndex(0);
+      console.log(pages);
+    }
+  };
+
+  useEffect(() => {
+    console.log(index);
+    const newIndex = index < 0 ? index + 7 : index;
+    console.log(newIndex);
+    setBookIndex(pages[newIndex]);
+  }, [index]);
+
+  const nextPrevPagesHandler = (isForward) => {
+    if (isForward) {
+      setIndex((prevState) => (prevState + 1) % 7);
+    } else {
+      setIndex((prevState) => (prevState - 1) % 7);
+      //   setBookIndex(pages[index]);
+    }
   };
 
   return (
     <div>
-      <Backdrop show={isOpen} onClick={clickHandler} />
+      <Backdrop show={isOpen} onClick={() => clickHandler()} />
       <div
         className={classes.Modal}
         style={{
@@ -24,10 +66,18 @@ const Materialy = () => {
           opacity: isOpen ? "1" : "0",
         }}
       >
-        <img src={bookIndex} alt="my guitar book" />
+        <div className={classes.RightLeftButtons}>
+          <div onClick={() => nextPrevPagesHandler(false)}>{`<`}</div>
+          <div onClick={() => nextPrevPagesHandler(true)}>{`>`}</div>
+        </div>
+        <img src={bookIndex} alt="my guitar book"></img>
       </div>
       <div className={classes.Main} style={{ maxWidth: "80%" }}>
         <h1>Materiały</h1>
+        <span style={{ marginBottom: "1rem" }}>
+          ** Zamówić podręczniki można przez telefon lub e-mail. Wysyłka
+          InPostem w dzień potwierdzenia opłaty.
+        </span>
         <ul className={classes.Materialy}>
           <a href="#foo" onClick={() => clickHandler(one_book)}>
             <p>60zł</p>
@@ -51,7 +101,7 @@ const Materialy = () => {
             <p>60zł</p>
             <img src={one_book_03} alt="my guitar book" />
             <div>
-              <h3>Podręcznik "My Guitar Book II"</h3>
+              <h3>Podręcznik "My Guitar Book III"</h3>
               <span>+podkłady. Gitarowe solówki.</span>
             </div>
           </a>
--- src/ZapiszSie/ZapiszSie.js
+++ src/ZapiszSie/ZapiszSie.js
@@ -9,6 +9,11 @@ const ZapiszSie = () => {
       <div className={classes.Photo}></div>
       <div className={homeClasses.Home_content}>
         <h1>Wybierz Dowolny Termin</h1>
+        <span className={homeClasses.remark}>
+          ** Zapisać się na zajęcia można przeż telefon:{" "}
+          <a href="tel:+(48)732-188-593">+(48)732-188-593 </a>
+          albo e-mail: <a href="mailto:eshurmy@gmail.com">eshurmy@gmail.com</a>
+        </span>
         <Calendar locale="PL-pl" calendarType={"ISO 8601"} />
       </div>
     </div>
